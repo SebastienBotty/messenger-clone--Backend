574be3f815e25063fc9289704d9117d87b36e6c0
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-05-18 16:45:28 +0200

    feat Created webSocket server + .env .gitignore

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const http = require("http");
-const socketIo = require("socket.io");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 require("dotenv").config();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGO_URL;
-
+//------------------Express
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(
@@ -21,6 +18,24 @@ app.use(
   })
 );
 
+//------------------Web Socket
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+// WebSocket connection
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+
+//-----------------MongoDB
 // Connexion à la base de données
 mongoose.connect(MONGO_URL);
 
@@ -40,14 +55,6 @@ db.once("open", function () {
 module.exports = db;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-
-// WebSocket connection
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
 
 server.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:3000`)
