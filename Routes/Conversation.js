8fa582b79d8dc863abcdb6353622ffd1a706074f
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-05-30 19:05:51 +0200

    Feat Post conversation in user Model
    When a conversation is created, its ID is now put in conversations field
    of every members of it

--- Routes/Conversation.js
+++ Routes/Conversation.js
@@ -2,21 +2,31 @@ const express = require("express");
 const router = express.Router();
 const Conversation = require("../Models/Conversation");
 const Message = require("../Models/Message");
+const User = require("../Models/User");
 
 //----------------------POST---------------------------
 router.post("/", async (req, res) => {
   const members = req.body.members;
   const isGroupConversation = members.length > 2; //If there is more than 2 members when conversatoin is created, it is flagged as a group Conversation. Then the admin is the one who created the conversation
-  const admin = isGroupConversation ? req.body.admin : []; //If there is only 2, there's noe admin
+  //const admin = isGroupConversation ? req.body.admin : []; //If there is only 2, there's noe admin
+  const admin = req.body.admin;
   const conversation = new Conversation({
     isGroupConversation: isGroupConversation,
     members: members,
     admin: admin,
-    messages: req.body.messages,
+    messages: [],
     creationDate: req.body.creationDate,
   });
   try {
     const newConversation = await conversation.save();
+    for (let member of members) {
+      const user = await User.findOne({
+        userName: new RegExp("^" + member + "$", "i"),
+      });
+      user.conversations.push(newConversation._id);
+      await user.save();
+    }
+
     res.status(201).json(newConversation);
   } catch (error) {
     res.status(400).json({ message: error.message });
