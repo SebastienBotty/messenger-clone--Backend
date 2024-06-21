754add68a2e45d3ba2a998f899c05a2f56e78596
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2024-06-21 17:25:03 +0200

    feat: GET files by conversation ID and file names
    This commit introduces a new GET endpoint "/conversationId/:conversationId/getFiles" secured by authentication middleware. It retrieves files associated with a conversation ID and specified file names from AWS S3.
    
    The endpoint verifies the presence of file names in the query parameters, retrieves file metadata including MIME type, and generates signed URLs for downloading or previewing based on file type (image or other).
    
    If files are found, it responds with their details including download URLs or base64-encoded preview URLs for images. Error handling ensures proper responses for missing files or unexpected errors from S3.
    
    Changes:
    - Added route handler to fetch files by conversation ID and file names
    - Integrated AWS S3 operations for file retrieval and URL signing
    - Implemented error handling and response validation
    
    POST Files changes :
    post files on S3 with a date stamp in name to have unique filenames.
    Returns the stamped name to frontend.
    Put a contentType field in S3 params to differentiate the type of file

--- Routes/File.js
+++ Routes/File.js
@@ -12,10 +12,13 @@ AWS.config.update({
 });
 
 const s3 = new AWS.S3({});
+const bucketName = process.env.AWS_BUCKET_NAME;
 
 // Configure multer to use memory storage
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+});
 
 // Route to receive files from frontend
 router.post(
@@ -23,8 +26,9 @@ router.post(
   auth,
   upload.array("files"),
   async (req, res) => {
-    const bucketName = process.env.AWS_BUCKET_NAME;
     const convId = req.params.conversationId;
+    const fileNamesArr = [];
+    const timeStamp = Date.now();
     try {
       // Check if folder exists in S3, if not create it
       await createFolderInS3IfNotExists(bucketName, convId);
@@ -34,18 +38,109 @@ router.post(
         req.files.map(async (file) => {
           const params = {
             Bucket: bucketName,
-            Key: `${convId}/${file.originalname}`,
+            Key: `${convId}/${timeStamp}-${file.originalname}`,
             Body: file.buffer,
+            ContentType: file.mimetype,
           };
 
           await s3.upload(params).promise();
+
+          fileNamesArr.push(`${timeStamp}-${file.originalname}`);
         })
       );
 
-      res.status(200).json({ message: "Files uploaded successfully." });
+      res.status(200).json({ fileNames: fileNamesArr });
     } catch (err) {
       console.error("Error uploading files to S3:", err);
       res.status(500).send("Failed to upload files.");
+    }
+  }
+);
+
+// Route pour récupérer des fichiers spécifiques par leurs noms
+router.get(
+  "/conversationId/:conversationId/getFiles",
+  auth,
+  async (req, res) => {
+    const convId = req.params.conversationId;
+    const fileNames = req.query.fileNames.split(",");
+
+    console.log(convId);
+    console.log(fileNames);
+
+    if (!Array.isArray(fileNames) || fileNames.length === 0) {
+      return res.status(400).json({
+        error: "An array of file names is required in the query parameters.",
+      });
+    }
+
+    try {
+      const filesData = await Promise.all(
+        fileNames.map(async (fileName) => {
+          const params = {
+            Bucket: bucketName,
+            Key: `${convId}/${fileName}`,
+          };
+
+          try {
+            const data = await s3.headObject(params).promise();
+            const mimeType = data.ContentType;
+            console.log(data);
+            console.log(mimeType);
+
+            if (mimeType.startsWith("image/")) {
+              // For images, return the base64 encoded data
+              const signedImgUrl = s3.getSignedUrl("getObject", {
+                Bucket: bucketName,
+                Key: params.Key,
+              });
+
+              return {
+                fileName,
+                type: "image",
+                previewUrl: signedImgUrl,
+              };
+            } else {
+              // For other files, generate a signed URL for downloading
+              const signedUrl = s3.getSignedUrl("getObject", {
+                Bucket: bucketName,
+                Key: params.Key,
+                //Expires: 15,
+                ResponseContentDisposition: "attachment", // Indique au navigateur de télécharger le fichier plutôt que de l'afficher
+              });
+
+              return {
+                fileName,
+                type: "file",
+                downloadUrl: signedUrl,
+              };
+            }
+          } catch (err) {
+            console.error(`Error fetching file ${fileName} from S3:`, err);
+            return null; // Return null for files that couldn't be fetched
+          }
+        })
+      );
+
+      const filteredFilesData = filesData.filter((file) => file !== null);
+
+      if (filteredFilesData.length === 0) {
+        return res
+          .status(404)
+          .json({ error: "No files found matching the given names." });
+      }
+      filteredFilesData.forEach((file) => {
+        if (file.type === "file") {
+          res.set({
+            "Content-Disposition": `attachment; filename="${file.fileName}"`,
+          });
+        }
+      });
+
+      res.status(200).json({ files: filteredFilesData });
+    } catch (err) {
+      console.error("Error fetching files from S3:", err);
+      res.status(500).send("Failed to fetch files.");
     }
   }
 );
