bf05beab58648490ffe1503bf87cadfba5b787a6
SebastienBotty <74521922+SebastienBotty@users.noreply.github.com>
2025-04-28 21:35:42 +0200

    added portfolio url to cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ initSocket(server);
 app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
-    origin: [process.env.TOU_URL, process.env.FRONTEND_URL,],
+    origin: [process.env.TOU_URL, process.env.FRONTEND_URL, process.env.PORTFOLIO_URL],
   })
 );
 
